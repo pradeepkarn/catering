499c212eabf0cdd95854cbe376c741e50d6c4b34
pradeepkarn <mail2pkarn@gmail.com>
2023-11-14 14:08:15 +0530

    t

--- controllers/api/Event_api.api.php
+++ controllers/api/Event_api.api.php
@@ -97,45 +97,50 @@ class Event_api
             $managers = json_decode($event->managers ?? '[]');
             $employees = json_decode($event->employees ?? '[]');
             $employeesList = $this->get_employee_details($db=$this->db, $idList=$employees);
-            $managersList = $this->get_employee_details($db=$this->db, $idList=$employees);
-            if (in_array($myid, $managers)) {
-                $unique_employees = array_unique(array_merge($managers, $employees));
-                $am_i_assigned = false;
-                $assigned_as = "NA";
-                if ($myid) {
-                    $am_i_assigned = in_array($myid, $unique_employees);
-                    if (in_array($myid, $managers) && in_array($myid, $employees)) {
-                        $assigned_as = "manager, employee";
-                    } else if (!in_array($myid, $managers) && in_array($myid, $employees)) {
-                        $assigned_as = "employee";
-                    } else if (in_array($myid, $managers) && !in_array($myid, $employees)) {
-                        $assigned_as = "manager";
-                    } else if (!in_array($myid, $managers) && !in_array($myid, $employees)) {
-                        $assigned_as = "NA";
-                        $am_i_assigned = false;
-                    }
-                }
-                $unique_employee_count_with_managers = count($unique_employees);
+            $empls = null;
+            array_map(function($d){
+                $d['image'] = dp_or_null($d['image']);
+                $empls[] = $d;
+            },$employeesList);
+            // $managersList = $this->get_employee_details($db=$this->db, $idList=$employees);
+            // if (in_array($myid, $managers)) {
+                // $unique_employees = array_unique(array_merge($managers, $employees));
+                // $am_i_assigned = false;
+                // $assigned_as = "NA";
+                // if ($myid) {
+                //     $am_i_assigned = in_array($myid, $unique_employees);
+                //     if (in_array($myid, $managers) && in_array($myid, $employees)) {
+                //         $assigned_as = "manager, employee";
+                //     } else if (!in_array($myid, $managers) && in_array($myid, $employees)) {
+                //         $assigned_as = "employee";
+                //     } else if (in_array($myid, $managers) && !in_array($myid, $employees)) {
+                //         $assigned_as = "manager";
+                //     } else if (!in_array($myid, $managers) && !in_array($myid, $employees)) {
+                //         $assigned_as = "NA";
+                //         $am_i_assigned = false;
+                //     }
+                // }
+                // $unique_employee_count_with_managers = count($unique_employees);
 
-                $myevent = array(
+                // $myevent = array(
                     // 'id' => $event->id,
                     // 'title' => $event->title,
                     // 'logo' => img_or_null($event->banner),
                     // 'address' => $event->address,
                     // 'event_datetime' => strval(strtotime($event->event_date . " " . $event->event_time)),
                     // 'number_of_employees' => $unique_employee_count_with_managers,
-                    'employees'=>$employeesList,
+                    // 'employees'=>$employeesList,
                     // 'am_i_assigned' => $am_i_assigned,
                     // 'assgined_as' => $assigned_as,
-                );
-            }
+                // );
+            // }
         }
-        return  $myevent;
+        return  $empls;
     }
     function get_employee_details($db, $idList)
     {
         $escapedIds = implode(',', array_map('intval', $idList));
-        $sql = "SELECT id, CONCAT(first_name, ' ', last_name) AS name, email FROM pk_user WHERE id IN ($escapedIds)";
+        $sql = "SELECT id, CONCAT(first_name, ' ', last_name) AS name, email, image FROM pk_user WHERE id IN ($escapedIds)";
         return $db->show($sql);
     }
     function get_all_events($myid = null)
