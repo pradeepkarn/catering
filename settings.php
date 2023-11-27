4a4abf018be7e03fd18e6a2cad3153e23be1f482
pradeepkarn <mail2pkarn@gmail.com>
2023-11-27 11:12:44 +0530

    user group change in settings

--- settings.php
+++ settings.php
@@ -34,7 +34,7 @@ const FOOD_CATEGORY = [
     1=>"Junior",
     2=>"Senior"
 ];
-const USER_GROUP_LIST = ['caterer', 'user'];
+const USER_GROUP_LIST = ['employee', 'manager'];
 const ADMIN_GROUP_LIST = ['superuser', 'admin', 'subadmin'];
 const PERMISSION_GROUP_LIST = ['all', 'employee', 'event', 'subadmin'];
 // employee positions
